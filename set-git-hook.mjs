e19aa89c60642fc97883201e0f513d627c888241
WhyK <windchimeyk@gmail.com>
2021-08-28 02:18:35 +0900

    fix: update set-git-hook.mjs

--- set-git-hook.mjs
+++ set-git-hook.mjs
@@ -1,8 +1,7 @@
-// コード内の実行権限付与コマンドがWindowsで動作しないので、WindowsでGit Hooksが動作するかは未知数
+// こんなにいらないかも
 const hooksPath = (await nothrow($`git config --local core.hooksPath`)).stdout;
 
 await $`npx rimraf .git/hooks/commit-msg`; // 既存Hooksの削除
 if (hooksPath !== ".githook\n") await $`git config --local core.hooksPath .githook` // Hooksフォルダの登録
 await $`git init` // カスタムHooksをリポジトリ設定に反映
-if (os.platform() === 'darwin') await $`chmod +x .githook/commit-msg` // カスタムHooksに実行権限を付与
 console.log(`${chalk.green('SUCCESS')} Command Complete!!`);
